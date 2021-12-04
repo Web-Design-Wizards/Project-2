1fa0041fc05241697dcdc72538f7b04dc150014b
Adrian <32913236+adrigski@users.noreply.github.com>
2021-12-04 02:33:59 -0600

    added pictures into images folder

--- js/site.js
+++ js/site.js
@@ -1,37 +1,37 @@
 // Splide Image Slider
 window.addEventListener('load', function() {
-    var splide = new Splide( '.splide', {
-        height: '20em' ,
-        width: '80%',
-        trimSpace: true,
-        cover: true,
-        heightRatio: 0.5,
-        type: 'fade',
-        autoplay: true,
-        rewind: true,
-        
+  var splide = new Splide( '.splide', {
+    height: '20em' ,
+    width: '80%',
+    trimSpace: true,
+    cover: true,
+    heightRatio: 0.5,
+    type: 'fade',
+    autoplay: true,
+    rewind: true,
+  });
+  splide.mount();
+});
 
-    });
-    splide.mount();
-  } );
+// Signup Button
+var signBtn = document.getElementById("signupBtn");
 
-  var signBtn = document.getElementById("signupBtn");
+signBtn.addEventListener('click', function() {
+  location.href = "http://example.com/"
+})
 
-  signBtn.addEventListener('click', function() {
-    location.href = "http://example.com/"
-  })
-
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
   
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
