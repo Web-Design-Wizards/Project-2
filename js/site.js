8a4ce3bf692c09dff12e20bdb618a2be9ba9e3b2
Adrian <32913236+adrigski@users.noreply.github.com>
2021-12-04 00:15:35 -0600

    troubleshooting js image slider by temporarily removing code for accordion

--- js/site.js
+++ js/site.js
@@ -10,17 +10,3 @@ document.addEventListener( 'DOMContentLoaded', function() {
     splide.mount();
   } );
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
